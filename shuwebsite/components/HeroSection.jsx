ec6f283cad2aea2939f5f76adfb73b9648f65d94
ohshuzanne <suzannelts02@gmail.com>
2023-12-08 21:11:30 +0800

    Added public folder

--- shuwebsite/components/HeroSection.jsx
+++ shuwebsite/components/HeroSection.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import TypeWritter from "typewriter-effect";
 import { motion } from "framer-motion";
+import Link from "next/link";
 
 const HeroSection = () =>{
     return(
@@ -34,16 +35,29 @@ const HeroSection = () =>{
                                 .typeString`<span style="color:#9E78FF">AI Engineer</span>`
                                 .pauseFor(1500)
                                 .deleteAll()
+                                .start();
                             }}
                               >
 
                             </TypeWritter>
                         </span>
+                        <p className="text-sky-100 mt-8 text-base sm:text-lg md:text-xl mb-6">Slide down to know more about me!</p>
+                        <div className = "flex space-x-5">
+                        <Link 
+                            href="https://www.linkedin.com/in/suzanne-lai-tzi-syuen-034103254/"
+                            className="px-6 inline-block py-3 rounded-full border-2 border-pink-300 hover:bg-pink-500">
+                            LinkedIn
+                        </Link>
+                        <Link
+                            href="https://drive.google.com/file/d/1IjwcDidXCKCkSzSCtbBNTWkNvKF0keh4/view?usp=drive_link"
+                            className="px-6 inline-block py-3 rounded-full border-2 border-indigo-300 hover:bg-indigo-500">
+                            Resume
+                        </Link>
+                        </div>
                     </h1>
                 </motion.div>
             </div>
         </main>
     );
 };
-
 export default HeroSection;
