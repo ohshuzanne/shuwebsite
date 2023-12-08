fb2608b096ef48559397da70ab451cd40edc976e
ohshuzanne <suzannelts02@gmail.com>
2023-12-08 19:55:03 +0800

    added components

---
+++ shuwebsite/components/HeroSection.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import TypeWritter from "typewriter-effect";
+
+const HeroSection = () =>{
+    return(
+        <main className="py-16 mt-16 md:flex md: shirink-0">
+            <div className="grid grid-cols-1 md:grid-cols-12">
+                <div className="col-span-1 md:col-span-8 place-self-center justify-self-start">
+                    <hi className="font-extrabold">
+                        <span className="text-indigo-500 text-8xl">
+                            
+                        </span>
+                    </hi>
+                </div>
+            </div>
+        </main>
+    );
+};
+
+export default HeroSection;
--- shuwebsite/pages/index.js
+++ shuwebsite/pages/index.js
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function Home(){ //default home
-  return 
+  return (
+    <div class="dalton">
+      Hello Dalton! I'm Penis!
+    </div>
+  );
 }
